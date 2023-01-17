b87d1874cd7fcbd092ca0f6cf67f5dcb103b5900
jasonhilimire <jasonhilimire@mac.com>
2023-01-16 20:00:38 -0700

    moved date q

--- post_workout.py
+++ post_workout.py
@@ -4,22 +4,18 @@ from setup import PostEvent
 now = datetime.now()
 
 
-def get_date(date: str):
-    if date == "y":
+def get_date():
+    input_q = input("Use Today's Date?: y/n: ").lower()
+    if input_q == "y":
         date_string = now.strftime("%Y%m%d")
         return date_string
-    elif date == 'n':
+    elif input_q == 'n':
         short_date = input("Enter date in MMdd: ").lower()
-        date = f"2022{short_date}"
+        date = f"2023{short_date}"
         return date
 
 
-DATE = input("Use Today's Date?: y/n: ").lower()
-
 TYPE = input("Enter workout type: c for cycling-w for others: ").lower()
 
-the_date = get_date(DATE)
-post = PostEvent(workout=TYPE, date=the_date)
+post = PostEvent(workout=TYPE, date=get_date())
 post.create_post()
-
-
